e58c86ad3de820fa57e35ac13e0265b824d24ff4
AyushKumar <ayushkumar32339@gmail.com>
2025-05-05 19:21:52 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,8 @@
+function checkpassword() {
+    let password = prompt("Enter password:");
+    if (password === "1234") {
+        alert("Correct password");
+    } else {
+        alert("Incorrect password");
+    }
+}
